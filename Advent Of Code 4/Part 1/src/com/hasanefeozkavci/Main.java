60cb5f3d74fb18039705f79dc4f8333bb04468a9
Hasan Efe Özkavcı <hasanefeozkavci@hasans-macbook-pro.local>
2019-12-19 12:46:00 +0300

    Part1 is done.

--- Advent Of Code 4/Part 1/src/com/hasanefeozkavci/Main.java
+++ Advent Of Code 4/Part 1/src/com/hasanefeozkavci/Main.java
@@ -7,29 +7,30 @@ public class Main {
 	    int[] digit = {0,0,0,0,0,0};
 	    boolean test1 = true, test2 = false;
 	    for ( i = 231832; i <= 767346; i++) {
+	    	test1 = true;test2 = false;
 			for (k = 0; k < 6; k++) {
 				digit[k] = (int) Math.floor((i % Math.pow(10, k + 1)) / (Math.pow(10, k)));
 			}
 
 	        for(k = 0; k<5; k++){
-	            if ( digit[k] >= digit[k+1])
+	            if ( digit[k] > digit[k+1])
 	                test1 = false;
 
             }
-	        for(m = 0; m < 6; m++){
-	            for(n = 0; n < 6; n++){
-	                if(digit[m] == digit[n])
-	                    temp ++;
-                }
+	        for(m = 0; m < 5; m++){
+	           	if(digit[m] == digit[m+1])
+	           		temp++;
+	           	//System.out.println("temp: "+temp);
             }
+	        if( temp > 0)
+	        	test2 = true;
+	        temp = 0;
 			//System.out.println("Temp is:"+temp);
-	        if ( temp == 2) {
-				test2 = true;
-			}
+			System.out.println("test1: "+test1+" test2: "+test2);
             if ( test1&&test2) {
 				result++;
 			}
-            test1 = false; test2 = false; temp = 0;
+            test1 = false; test2 = false;
         }
 			System.out.println("Result: " + result);
     }
